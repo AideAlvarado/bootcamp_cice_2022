82de065ac25c7b0d1b1839cabf05d1307a1114d1
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-19 21:19:54 +0100

    Debugged errors on navigation controller
    Fixed errors

--- 18012022/MiAppAlertas/MiAppAlertas/Sources/InitialView/InitialViewController.swift
+++ 18012022/MiAppAlertas/MiAppAlertas/Sources/InitialView/InitialViewController.swift
@@ -8,25 +8,25 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-    
-    // MARK: - IBActions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   // MARK: - IBActions
     
 
     @IBAction func showAlertViewController(_ sender: UIButton) {
       
         switch sender.tag {
         case 0:
-         let alertVC = UIAlertController(title: "Esta es una alerta simple",
-                                         message: "Vamos a probar muchas cosas",
-                                         preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-                let storyboardNavigationViewController = self.storyboard?.instantiateInitialViewController(identifier: "NavigationViewController")
-
-                if let vc = storyboardNavigationViewController          {
+         let alertVC = UIAlertController(title: "Esta es una alerta simple",message: "Vamos a probar muchas cosas", preferredStyle: .alert)
+         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+           let storyboardNavigationViewController = self.storyboard?.instantiateViewController(identifier: "NavigationViewController")
+           if let vc = storyboardNavigationViewController {
                     self.show(vc , sender: nil)
-                
                 }
-                
             }
             alertVC.addAction(okAction)
             
@@ -44,53 +44,44 @@ class InitialViewController: UIViewController {
             let actionSheetVC = UIAlertController(title: "Esta es una hoja de acciones",
                                                   message: "Vamos a seguir probando todo",
                                                   preferredStyle: .actionSheet)
-            actionSheetVC.addAction(UIAlertAction(title: "Default", style: .default, handler: { _ in
-                self.choosePhotoLibrary()
-            }))
+            actionSheetVC.addAction(UIAlertAction(title: "Default",
+                                                  style: .default,
+                                                  handler: { _ in  self.choosePhotoLibrary() }
+                                                 )
+                                   )
             
-            actionSheetVC.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
-                let storyboardNavigationViewController = self.storyboard?.instantiateInitialViewController(identifier:"NavigationViewController")
+            actionSheetVC.addAction(UIAlertAction(title: "Cancel",
+                                                  style: .cancel ,
+                                                  handler: { _ in                                                                let storyboardNavigationViewController = self.storyboard?.instantiateViewController(identifier:"NavigationViewController")
                                
-                if let vc = storyboardNavigationViewController          {
-                    self.show(vc , sender: nil)
+                                                                if let vc = storyboardNavigationViewController  {self.show(vc , sender: nil) }
+                                                            }
+                                                        ))
                 
-            }))
+            actionSheetVC.addAction(UIAlertAction(title: "delete",
+                                                  style: .destructive,
+                                                  handler: { _ in
+                let storyboardPresenterViewController = self.storyboard?.instantiateViewController(identifier: "PresenterViewController")
+                if let vc = storyboardPresenterViewController  { self.show(vc , sender: nil)}
                 
-                actionSheetVC.addAction(UIAlertAction(title: "delete", style: .destructive, handler: { _ in
-                    let storyboardPresenterViewController =
-                    self.storyboard?.instantiateInitialViewController(creator:PresenterViewController())
-                    if let vc = storyboardNavigationViewController          {
-                        self.show(vc , sender: nil)
-                }))
+                                                            } // handler
+                                                 ) //UIAlertAction
+                                                )
                 self.present(actionSheetVC, animated: true)
                 // TODO: - AUI METERMOS OTRO METODO
                 
-            }
+            } //switch
                                                   
-            }
-        
-    }
+    } //Function
+    private func choosePhotoLibrary(){
+        let imagePickerController = UIImagePickerController()
+                   imagePickerController.sourceType = .photoLibrary
+                   imagePickerController.delegate = self
+                   self.present(imagePickerController, animated: true, completion: nil)
+                   
+               }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-     private func choosePhotoLibrary(){
-         let imagePickerController = UIImagePickerController()
-                    imagePickerController.sourceType = .photoLibrary
-                    imagePickerController.delegate = self
-                    self.present(imagePickerController, animated: true, completion: nil)
-                    
-                }
-
 }
-                                                      
+
                                                     
-  extension InitialViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-                    
-                    
-                }
+extension InitialViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {}
