165927d6e4cea64f987eb0e68731e02633a982a9
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-19 20:43:19 +0100

    updated

--- 18012022/MiAppAlertas/MiAppAlertas/Sources/InitialView/InitialViewController.swift
+++ 18012022/MiAppAlertas/MiAppAlertas/Sources/InitialView/InitialViewController.swift
@@ -79,14 +79,18 @@ class InitialViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     private func choosePhotoLibrary(){
+         let imagePickerController = UIImagePickerController()
+                    imagePickerController.sourceType = .photoLibrary
+                    imagePickerController.delegate = self
+                    self.present(imagePickerController, animated: true, completion: nil)
+                    
+                }
 
 }
+                                                      
+                                                    
+  extension InitialViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+                    
+                    
+                }
