08545b53f642807f1f4bdeb0e8a16aaad72ab5eb
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-19 18:51:24 +0100

    [INIT] MiAppAlertas

---
+++ 18012022/MiAppAlertas/MiAppAlertas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiAppAlertas
+//
+//  Created by Aide Alvarado on 19/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
