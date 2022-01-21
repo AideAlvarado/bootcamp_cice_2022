52d946fd1ee603e449cc3d97901ef2dba288930d
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-21 19:27:24 +0100

    [INIT-PROJECT] - Created New App Mi Data Navigation

---
+++ 20012022/MiDataNavigation/MiDataNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiDataNavigation
+//
+//  Created by Aide Alvarado on 21/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
