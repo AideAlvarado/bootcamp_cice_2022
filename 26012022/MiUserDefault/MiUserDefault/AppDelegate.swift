0f32df361b203d22b71ec725de90c4ad59cc113a
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-27 21:25:10 +0100

    [INIT] MiUserDefault App_ 26012022
    New Project

---
+++ 26012022/MiUserDefault/MiUserDefault/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MiUserDefault
+//
+//  Created by Aide Alvarado on 27/1/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
