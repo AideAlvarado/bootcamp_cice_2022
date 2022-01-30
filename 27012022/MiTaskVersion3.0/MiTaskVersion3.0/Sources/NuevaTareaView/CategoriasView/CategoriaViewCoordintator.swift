b9b1e349ea667de817ddc5c265d419f6c7e2a1c4
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-30 17:31:36 +0100

    [WIP] Added coordinator class

---
+++ 27012022/MiTaskVersion3.0/MiTaskVersion3.0/Sources/NuevaTareaView/CategoriasView/CategoriaViewCoordintator.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriaViewCoordintator.swift
+//  MiTaskVersion3.0
+//
+//  Created by Aide Alvarado on 30/1/22.
+//
+
+import UIKit
+
+final class CategoriaViewCoordinator {
+    
+    static func navigation() -> UINavigationController {
+        let navVC = UINavigationController(rootViewController: view())
+        return navVC
+    }
+    
+    static func view() -> UIViewController {
+        let vc = CategoriaViewController()
+        return vc
+    }
+}
