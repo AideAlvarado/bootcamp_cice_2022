cdc4d184c2c7a1db608edafb81ddac7f86f4544c
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-01-30 17:11:37 +0100

    [INIT_PROJECT] Created new project Task3.0

---
+++ 27012022/MiTaskVersion3.0/MiTaskVersion3.0/CoreApp/AppCoreCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  AppCoreCoordinator.swift
+//  MiTaskVersion3.0
+//
+//  Created by Aide Alvarado on 30/1/22.
+//
+
+import UIKit
+
+protocol AppCoreCoordinatorProtocol {
+    func initialViewController(window: UIWindow)
+}
+
+final class AppCoreCoordinator {
+    
+    var actualVC = UIViewController()
+    
+}
+
+extension AppCoreCoordinator: AppCoreCoordinatorProtocol {
+    func initialViewController(window: UIWindow) {
+        self.actualVC = UIViewController()
+        window.rootViewController = self.actualVC
+        window.makeKeyAndVisible()
+    }
+}
