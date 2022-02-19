61271c83e83ad0e5b0dbafcb0741b7bb54d1f288
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-02-19 18:39:51 +0100

    [WIP] FashionApp - SwiftUI

---
+++ 17022022/FashionApp/FashionApp/Components/HeaderComponents.swift
@@ -0,0 +1,54 @@
+//
+//  HeaderComponents.swift
+//  FashionApp
+//
+//  Created by Aide Alvarado on 19/2/22.
+//
+
+import SwiftUI
+
+struct HeaderComponentes: View {
+    var body: some View {
+        VStack{
+            titleView(title: "Header Component")
+            subtitleView(subtitle: "Estamos aprendiendo SwiftUI, con Xcode 12 y 13 para mejorar nuestra comprensión del mundo Apple")
+        }
+    }
+    
+}
+
+struct titleView: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .padding(20)
+            .font(.title)
+            .background(Color.pink)
+            .foregroundColor(.black)
+            .cornerRadius(20)
+    }
+}
+
+struct subtitleView: View {
+    
+    var subtitle: String
+    
+    var body: some View {
+        Text(subtitle)
+            .padding(.horizontal, 20)
+            .font(.title3)
+            .foregroundColor(.black)
+    }
+}
+
+
+
+/*
+struct HeaderComponentes_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderComponentes()
+    }
+}*/
+
---
+++ 17022022/FashionApp/FashionApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FashionApp
+//
+//  Created by Aide Alvarado on 19/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            HeaderComponentes()
+            Text("Holi Aidely!")
+                .font(.title)
+        }
+    }
+}
+
+/*
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}*/
+
