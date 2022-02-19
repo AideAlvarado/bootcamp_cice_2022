30ab49b41c8718e6fccf2e81153879ddb91d5507
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-02-19 17:37:49 +0100

    [WIP] Created Tips Module

--- 04022022/CiceMusic/CiceMusic/CoreApp/AppDelegate.swift
+++ 04022022/CiceMusic/CiceMusic/CoreApp/AppDelegate.swift
@@ -11,16 +11,27 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    // MARK: - Variables globales
     var window: UIWindow?
     let appCore: AppCoreCoordinatorProtocol = AppCoreCoordinator()
+    
+    var canRotate = false
+    //var orientationEnable: UIInterfaceOrientationMask = .portrait
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        // aqui definimos el controlador con el que vamos a trabajar:
         self.window = UIWindow(frame: UIScreen.main.bounds)
         if let windowUnw = self.window {
             self.appCore.initialViewController(window: windowUnw)
         }
         return true
+    }
+    
+    // MARK: - Autorotations
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if canRotate {
+            return .all
+        } else {
+            return .portrait
+        }
     }
 }
