c5486195ab27f027f25e094787810c38aecf9942
Aide Alvarado <78866513+AideAlvarado@users.noreply.github.com>
2022-02-25 19:08:10 +0100

    [WIP] MoviesView, Components

--- 22022022/CiceTmdbApp/CiceTmdbApp/Sources/MoviesView/MoviesView.swift
+++ 22022022/CiceTmdbApp/CiceTmdbApp/Sources/MoviesView/MoviesView.swift
@@ -9,12 +9,41 @@ import SwiftUI
 
 struct MoviesView: View {
     
-    @ObservedObject var viewModel: MoviesViewModel
+    @StateObject var viewModel = MoviesViewModel()
     
     var body: some View {
-        VStack{
-            Text("2")
+        List{
+            Group{
+                if !self.viewModel.dataSourceNowPlaying.isEmpty {
+                    MoviesPosterCarrousel(title: "Now Playing",
+                                          moviesModel: self.viewModel.dataSourceNowPlaying,
+                                          isPoster: false)
+                }
+            }
+            Group{
+                if !self.viewModel.dataSourcePopular.isEmpty {
+                    MoviesPosterCarrousel(title: "Popular",
+                                          moviesModel: self.viewModel.dataSourcePopular,
+                                          isPoster: true)
+                }
+            }
+            Group{
+                if !self.viewModel.dataSourceTopRate.isEmpty {
+                    MoviesPosterCarrousel(title: "Top Rate",
+                                          moviesModel: self.viewModel.dataSourceTopRate,
+                                          isPoster: false)
+                }
+            }
+            Group{
+                if !self.viewModel.dataSourceUpcoming.isEmpty {
+                    MoviesPosterCarrousel(title: "UpComing",
+                                          moviesModel: self.viewModel.dataSourceUpcoming,
+                                          isPoster: true)
+                }
+            }
         }
+        .listStyle(PlainListStyle())
+        .navigationTitle(Text("Movies"))
         .onAppear {
             self.viewModel.fetchData()
         }
